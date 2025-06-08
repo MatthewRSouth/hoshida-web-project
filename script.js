bcf15e8dc67dace205535b3261156d84dc9eb729
Matthew South <matthewsouth@Matthews-MacBook-Pro.local>
2025-06-08 14:07:34 +0900

    Fix: Rename assets and implement resilient CSS for carousel

--- script.js
+++ script.js
@@ -4,17 +4,26 @@ carousel();
 
 function carousel() {
     //function cycles through the images of the carosoul
-    let i;
-    let x = document.querySelectorAll('.mySlides');
-    for (i = 0; i < x.length; i++) {
-        x[i].classList.remove('showing');
+    // Replace your entire carousel function with this one
+
+    let myIndex = 0;
+    carousel();
+
+    function carousel() {
+        let i;
+        let x = document.querySelectorAll('.mySlides');
+        for (i = 0; i < x.length; i++) {
+            // Remove 'showing' from all slides
+            x[i].classList.remove('showing');
+        }
+        myIndex++;
+        if (myIndex > x.length) {
+            myIndex = 1;
+        }
+        // Add 'showing' to only the current slide
+        x[myIndex - 1].classList.add('showing');
+        setTimeout(carousel, 3000); // Change image every 3 seconds
     }
-    myIndex++;
-    if (myIndex > x.length) {
-        myIndex = 1;
-    }
-    x[myIndex - 1].classList.add('showing');
-    setTimeout(carousel, 3000); //change every 3 seconds
 }
 
 //objects holding Japanese and English translations.
